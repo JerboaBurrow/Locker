2a579a6468e97cc1b7dcd17a0181ddafa13b552a
Jerboa-app <dev@jerboa.app>
2023-11-10 14:53:36 +0000

    additional test

--- tests/test_encrypt_decrypt.rs
+++ tests/test_encrypt_decrypt.rs
@@ -14,4 +14,21 @@ mod test_encrypt_decrypt
         // 0's will be padded to dec_result
         assert_eq!(&dec_result[0..TEST_STRING.len()], TEST_STRING.as_bytes());
     }
+
+    #[test]
+    fn cipher_text_is_different()
+    {
+        let rsa = build_rsa("tests/donotuse.pem", "password");
+        let enc_result_a = encrypt(rsa.clone(), TEST_STRING.as_bytes());
+        let enc_result_b = encrypt(rsa.clone(), TEST_STRING.as_bytes());
+        assert_ne!(enc_result_a, enc_result_b);
+
+        let dec_result_a = decrypt(rsa.clone(), &enc_result_a);
+        let dec_result_b = decrypt(rsa.clone(), &enc_result_b);
+
+        assert_eq!(dec_result_a, dec_result_b);
+
+        assert_eq!(&dec_result_a[0..TEST_STRING.len()], TEST_STRING.as_bytes());
+        assert_eq!(&dec_result_b[0..TEST_STRING.len()], TEST_STRING.as_bytes());
+    }
 }
