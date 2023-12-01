c30da2c919cf5301b81eb240d360cc318833f0f6
Jerboa-app <dev@jerboa.app>
2023-12-01 12:31:22 +0000

    adds --re_key

--- src/command.rs
+++ src/command.rs
@@ -1,7 +1,7 @@
 use crate::
 {
     error::{CommandError, CommandResult},
-    file::Locker, crypto::generate_key, arguments::extract_pass
+    file::Locker, crypto::{generate_key, build_rsa}, arguments::extract_pass
 };
 
 use std::path::Path;
@@ -16,7 +16,9 @@ use openssl::
 pub enum CommandCode
 {
     ShowKeys,
-    GenKey
+    GenKey,
+    ReKey,
+    Export
 }
 
 #[derive(Debug, Clone)]
@@ -44,14 +46,73 @@ pub fn extract_command(args: &mut Vec<String>) -> Result<Option<Command>, Comman
         if i+1 < args.len()
         {
             let s = args[i+1].parse::<String>().unwrap();
-            args.remove(i);   
-            args.remove(i);
-            return Ok(Some(Command { code: CommandCode::GenKey, argument: Some(s), data: extract_pass(args)}));
+            if s.find("-").is_none()
+            {
+                args.remove(i);   
+                args.remove(i);
+                return Ok(Some(Command { code: CommandCode::GenKey, argument: Some(s), data: extract_pass(args)}));
+            }
+            else 
+            {
+                args.remove(i);
+                return Ok(Some(Command { code: CommandCode::GenKey, argument: None, data: extract_pass(args)}));
+            }
         }
         else
         {
             args.remove(i);
             return Ok(Some(Command { code: CommandCode::GenKey, argument: None, data: extract_pass(args)}));
+        }
+    }
+
+    if args.iter().any(|x| x == "--re_key")
+    {
+        let i = args.iter().position(|x| x == "--re_key").unwrap();
+
+        if i+2 < args.len()
+        {
+            let s = args[i+1].parse::<String>().unwrap();
+            let p = args[i+2].parse::<String>().unwrap();
+            
+            if s.find("-").is_none() && p.find("-").is_none()
+            {
+                args.remove(i);   
+                args.remove(i);
+                args.remove(i);
+                return Ok(Some(Command { code: CommandCode::ReKey, argument: Some(s), data: Some(p) }));
+            }
+            else if s.find("-").is_none()
+            {
+                args.remove(i);   
+                args.remove(i);
+                return Ok(Some(Command { code: CommandCode::ReKey, argument: Some(s), data: None }));
+            }
+            else 
+            {
+                args.remove(i);
+                return Ok(Some(Command { code: CommandCode::ReKey, argument: None, data: None }));
+            }
+            
+        }
+        if i+1 < args.len()
+        {
+            let s = args[i+1].parse::<String>().unwrap();
+            if s.find("-").is_none()
+            {
+                args.remove(i);   
+                args.remove(i);
+                return Ok(Some(Command { code: CommandCode::ReKey, argument: Some(s), data: None }));
+            }
+            else 
+            {
+                args.remove(i);
+                return Ok(Some(Command { code: CommandCode::ReKey, argument: None, data: None }));
+            }
+        }
+        else
+        {
+            args.remove(i);
+            return Ok(Some(Command { code: CommandCode::ReKey, argument: None, data: None }));
         }
     }
 
@@ -82,7 +143,85 @@ pub fn handle_command(lkr_path: &str, rsa: Rsa<Private>, command: Command) -> Re
         {
             show_keys(lkr_path, rsa)
         },
+        CommandCode::ReKey =>
+        {
+            re_key(lkr_path, rsa, command.argument, command.data)
+        },
         _ => {Ok(CommandResult::NOTHING_TO_DO)}
+    }
+}
+
+fn re_key(lkr_path: &str, old_rsa: Rsa<Private>, path: Option<String>, pass: Option<String>) -> Result<CommandResult, CommandError>
+{
+
+    match gen_key(path.clone(), pass.clone())
+    {
+        Ok(_r) => (),
+        Err(e) => return Err(e)
+    }
+
+    let mut old_lkr = Locker::new();
+    let mut lkr = Locker::new();
+
+    if !Path::new(lkr_path).exists()
+    {
+        return Err(CommandError { why: format!("Locker file {}, does not exit", lkr_path) });
+    }
+
+    match std::fs::copy(lkr_path, format!("{}.bk",lkr_path))
+    {
+        Ok(_) => {},
+        Err(why) => {return Err(CommandError{ why: format!("Error when backing up lkr file {} to {}.bk: {}", lkr_path, lkr_path, why)})}
+    }
+
+    match old_lkr.read(lkr_path)
+    {
+        Ok(_) => (),
+        Err(why) => 
+        {
+            return Err(CommandError { why: format!("{}", why) });
+        }
+    }
+
+    let pem = match path
+    {
+        Some(p) => p,
+        None => "locker.pem".to_string()
+    };
+
+    let password = match pass 
+    {
+        Some(s) => s,
+        None => 
+        {
+            rpassword::prompt_password
+            (
+                format!("Re enter passphrase for new key: ")
+            ).unwrap()
+        }
+    };
+
+    let rsa = match build_rsa(pem.as_str(), password.as_str())
+    {
+        Ok(v) => v,
+        Err(e) => 
+        {
+            println!("{}", e.why);
+            std::process::exit(1);
+        }
+    };
+
+
+    for key in old_lkr.get_keys(old_rsa.clone())
+    {
+        let value = old_lkr.get(&key, old_rsa.clone()).unwrap();
+        lkr.insert(&key, &value, rsa.clone(), true).unwrap();
+    }
+
+    match lkr.write(lkr_path)
+    {
+        Ok(_) => Ok(CommandResult::OK),
+        Err(why) => { Err(CommandError { why: format!("{}", why) }) }
     }
 }
 
--- tests/regressions/set_exisiting_key_overwrite.sh
+++ tests/regressions/set_exisiting_key_overwrite.sh
@@ -3,5 +3,3 @@ echo -e "set existing key k, f, p, o specified:\n"
 cp tests/test.lkr reg.lkr
 locker this_is_a_key abc -o --k tests/donotuse.pem --f reg.lkr --p password
 locker this_is_a_key --k tests/donotuse.pem --f reg.lkr --p password | grep "abc"
-
-rm reg.lkr reg.lkr.bk
